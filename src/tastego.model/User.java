a340dbedd7b67bf59b524f694668dd7e43b22c23
Dhana Lakshmi Arja <dhanalakshmiarja16@gmail.com>
2026-07-15 15:49:54 +0530

    User.java

---
+++ src/tastego.model/User.java
@@ -0,0 +1,107 @@
+package com.tastego.model;
+
+import java.sql.Timestamp;
+
+public class User {
+
+    private int userId;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+    private String address;
+    private String otp;
+    private boolean otpVerified;
+    private Timestamp createdAt;
+
+    public User() {
+    }
+
+    public User(int userId, String fullName, String email, String phone,
+            String password, String address, String otp,
+            boolean otpVerified, Timestamp createdAt) {
+
+        this.userId = userId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.address = address;
+        this.otp = otp;
+        this.otpVerified = otpVerified;
+        this.createdAt = createdAt;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public boolean isOtpVerified() {
+        return otpVerified;
+    }
+
+    public void setOtpVerified(boolean otpVerified) {
+        this.otpVerified = otpVerified;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
