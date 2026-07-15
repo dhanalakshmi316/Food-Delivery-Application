80986fb4c871dc6c15d75ff9dded1af2b4200363
Dhana Lakshmi Arja <dhanalakshmiarja16@gmail.com>
2026-07-15 15:33:58 +0530

    CDBConnect.java

---
+++ src/DBConnect.java
@@ -0,0 +1,37 @@
+package com.tastego.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/tastego";
+
+    private static final String USER =
+            "root";
+
+    private static final String PASSWORD =
+            "root";   // Change to your MySQL password
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Database Connected Successfully");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        return connection;
+    }
+}
